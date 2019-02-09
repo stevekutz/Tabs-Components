5e6a2c5be119598530f3a021be0a8410a14cbbcf
stevekutz <stkutz@gmail.com>
2019-02-09 11:04:27 -0600

    ADDED more comments & consoles to understand flow better

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,6 +1,6 @@
 class Dropdown {
   constructor(element) {
-    
+    console.log('INSIDE Dropdown ******  dropdown passed in as attr obj');
     // Assign this.element to the dropdown element
     this.element = element;
     console.log('this.element is ', this.element );
@@ -14,11 +14,14 @@ class Dropdown {
       console.log('this.content is ',  this.content );
 
     // Add a click handler to the button reference and call the toggleContent method.
+    //   the arrow function is using the implicit (automatic) binding to provide
+    //   value of 'this' where the arrow function was called.
+    //   We call this "lexical scope."
     this.button.addEventListener('click', () => this.toggleContent());
   }
 
   toggleContent() {
-    
+    // using 'this' allows toggleContent() to see context of this.content
     // Toggle the ".dropdown-hidden" class off and on
     this.content.classList.toggle('dropdown-hidden');
   }
@@ -26,4 +29,9 @@ class Dropdown {
 
 
 // Nothing to do here, just study what the code is doing and move on to the Dropdown class
-let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
+let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown =>
+    {
+      console.log('NEW Dropdown MADE, dropdown passed to UNHOISTED class  is ', dropdown);
+      new Dropdown(dropdown)
+    }
+);
--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -3,63 +3,74 @@ class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
     this.element = element;
-    console.log('element is ',this.element);
+    console.log('***********  INSIDE TabLink constructor');
+    console.log('element is the link passed in',this.element);
 
     // Get the custom data attribute on the Link
     this.data = this.element.dataset.tab;
-    console.log('this.data tabLink constructor is ', this.data);
+    console.log('this.data tab dataset value is ', this.data);
     
-    // Using the custom data attribute get the associated Item element
+    // Using the custom data attribute get the associated item element
     this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
-    console.log('this.itemElement inside tabLink constructor is ', this.itemElement);
+    console.log(`!!!! we matched dataset value ${this.data} to a  .tabs-item element `);
+    console.log('this.itemElement inside tabLink constructor is DOM element', this.itemElement);
 
     // Using the Item element, create a new instance of the TabItem class
     this.tabItem = new TabItem(this.itemElement);
-    console.log('this.tabItem inside of tabLink constructor is ', this.tabItem);
+    console.log('       this.tabItem declared  tabLink constructor is ', this.tabItem);
     
     // Add a click event listener on this instance, calling the select method on click
-    this.element.addEventListener('click', () => this.select() );
+    this.element.addEventListener('click', () => this.selectTab() );
 
   };
 
-  select() {
-    console.log('click fired, inside the select method');
+  selectTab() {
+    console.log('## click fired on tab, inside the select method to get TabItem to show the data');
 
     // Get all of the elements with the tabs-link class
     const links = document.querySelectorAll('.tabs-link');
     console.log('links inside TabLink select() method are ', links);
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    Array.from(links).forEach(link => link.classList.remove('tabs-link-selected') );
-    console.log('links is now ', links);
+    // Array.from(links).forEach(link => link.classList.remove('tabs-link-selected') ); // NOT NEEDED !!
+    links.forEach(link => link.classList.remove('tabs-link-selected') );
+
+    console.log('&&&&&  links is AFTER REMOVING tabs-links-selected is now ', links);
 
 
     // Add a class named "tabs-link-selected" to this link
     this.element.classList.add('tabs-link-selected');
-    console.log('this.element is', this.element);  // shows which is selected
+    console.log('@@ this.element with tabs-link-selected is', this.element);  // shows which is selected
     
     // Call the select method on the item associated with this link
-    this.tabItem.select();
+    this.tabItem.selectTabItemData();
 
   }
 }
 
 class TabItem {
   constructor(element) {
+    console.log('******  INSIDE TabItem *********************');
     // Assign this.element to the passed in element
     this.element = element;
-    console.log('this.element inside TabItem CONSTRUCTOR is ', this.element );
+    console.log('this.element passed to TabItem CONSTRUCTOR is ', this.element );
   }
 
-  select() {
+  selectTabItemData() {
+    console.log(`$$$$$$$$$$$   selectTabItemData called  on `, this.element);
+
+
     // Select all ".tabs-item" elements from the DOM
     const items = document.querySelectorAll('.tabs-item');
     console.log('INSIDE TabItem select() items is ', items);
 
     // Remove the class "tabs-item-selected" from each element
     //     ... SPREAD OPERATOR RULES !!!!!
-    [...items].forEach( item => item.classList.remove('tabs-item-selected') );
-    console.log('items after removal of tabs-item-selected is ', items );
+    //[...items].forEach( item => item.classList.remove('tabs-item-selected') );  // NOT NEEDED
+    items.forEach( item => item.classList.remove('tabs-item-selected') );
+
+         console.log('items after removal of tabs-item-selected is ', items );
+         console.log('%%%%%%%%%%%%%%%%%%   this.element is', this.element);
     
     // Add a class named "tabs-item-selected" to this element
     this.element.classList.add('tabs-item-selected');
@@ -76,5 +87,12 @@ class TabItem {
 
 */
 // let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
-links = document.querySelectorAll('.tabs-link').forEach(link => new TabLink(link));
-console.log('links from QSA is ', document.querySelectorAll('.tabs-link') );
+links = document.querySelectorAll('.tabs-link').forEach(link =>
+  {
+    console.log('******** new TabLink made, linked passed as attr obj to TabLink is ', link);
+    new TabLink(link)
+  }
+);
+
+// the QSA creates Nodelist
+  console.log('links from QSA is ', document.querySelectorAll('.tabs-link') );
